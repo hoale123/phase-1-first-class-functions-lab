e6eb6ba82e0c8499b28602c28cd170eacfdb38cd
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-01 23:50:32 -0400

    commit tree 97bb80b4245243a86c9cde9c8ac88607edbd2f1f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; // called element becuz its passing in the 2 function
+
+const createFareMultiplier = function(multiplyFare) {
+    return function (value) {
+        return multiplyFare * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
